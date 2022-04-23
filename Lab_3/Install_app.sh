fb4d637c8d75dcc3ea4639796955663abb9a58ff
Andriy125 <57405296+Andriy125@users.noreply.github.com>
2022-04-23 21:37:45 +0300

    Update Install_app.sh

--- Lab_3/Install_app.sh
+++ Lab_3/Install_app.sh
@@ -1,1 +1,1 @@
-sudo apt update && sudo apt upgrade -y && sudo apt install git openssh-server vim python3 git hwinfo htop -y && hwinfo	--short
+sudo apt update && sudo apt upgrade -y && sudo apt install git openssh-server vim python3 hwinfo htop -y && hwinfo	--short
